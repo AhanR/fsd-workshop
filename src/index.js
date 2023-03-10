79210f5abb1c4feba2cf1eff9e821736178c1971
Ahan <64554828+AhanR@users.noreply.github.com>
2023-03-10 14:49:08 +0530

    adding in the static files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import http from 'http'
+import http from 'http';
 import bodyParser from "body-parser";
+import path from "path";
 
 const router = express();
 
 router.use(bodyParser.json());
-router.get("/", (req, res, next) => res.json({"hi":"hello"}));
+router.get("/ping", (req, res, next) => res.json({"hi":"hello"}));
+router.use(express.static(process.cwd()+'/public'));
 
 http.createServer(router).listen(3000, ()=> console.log("server ready"));
