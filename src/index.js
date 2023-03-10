8047ffa2419b46c8761f7b27a2ff624a61cb9442
Ahan <64554828+AhanR@users.noreply.github.com>
2023-03-10 15:13:53 +0530

    adding in file upload support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
-import http from 'http'
+import http from 'http';
 import bodyParser from "body-parser";
+import path from "path";
+import multer from 'multer'
 
 const router = express();
+const upload = multer({dest:"files/"})
 
 router.use(bodyParser.json());
-router.get("/", (req, res, next) => res.json({"hi":"hello"}));
+router.post("/", upload.single('file') ,(req, res, next) => {
+    console.log(req.file.filename)
+    res.json({fileId : req.file.filename})
+});
+router.use(express.static(process.cwd()+'/public'));
 
 http.createServer(router).listen(3000, ()=> console.log("server ready"));
