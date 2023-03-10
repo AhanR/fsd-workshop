ac4ea139235bc0d8fe20bf4f405a5f778432da46
Ahan <64554828+AhanR@users.noreply.github.com>
2023-03-10 15:45:32 +0530

    added in database functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import http from 'http';
 import bodyParser from "body-parser";
-import path from "path";
 import multer from 'multer'
+import sqlite3 from "sqlite3";
 
 const router = express();
 const upload = multer({dest:"files/"})
+const sqlite = sqlite3.verbose();
+const db = new sqlite.Database(':memory:');
 
 router.use(bodyParser.json());
 router.post("/", upload.single('file') ,(req, res, next) => {
     console.log(req.file.filename)
-    res.json({fileId : req.file.filename})
+    db.run("INSERT INTO File VALUES ('"+req.file.filename+"','"+req.file.originalname+"')");
+    res.json({fileId : req.file.filename, filename: req.file.originalname})
 });
 router.use(express.static(process.cwd()+'/public'));
 
-http.createServer(router).listen(3000, ()=> console.log("server ready"));
+http.createServer(router).listen(3000, ()=> {
+    db.run("CREATE TABLE File( file_id varchar(40), file_name varchar(40))");
+    console.log("server ready");
+});
