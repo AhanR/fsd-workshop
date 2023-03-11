3adf5f08c1aed8443dd2719c7ee1b240b0e396d9
Ahan <64554828+AhanR@users.noreply.github.com>
2023-03-11 21:33:36 +0530

    completed basic functionality of the website

--- src/index.js
+++ src/index.js
@@ -15,8 +15,17 @@ router.post("/", upload.single('file') ,(req, res, next) => {
     db.run("INSERT INTO File VALUES ('"+req.file.filename+"','"+req.file.originalname+"')");
     res.json({fileId : req.file.filename, filename: req.file.originalname})
 });
-router.get("/download/:id", (req, res, next)=> {
-    res.sendFile('../file/'+req.params.id);
+router.get("/download/:id", async (req, res, next)=> {
+    let name = null;
+    await db.get("SELECT file_name as FN FROM File WHERE file_id = '"+req.params.id+"'", (e,row)=>{
+        console.log(row.FN);
+        name = row.FN;
+        if(name)
+        res.download('/files/'+req.params.id,"test.txt",{root: process.cwd()});
+        else res.json({message:"invalid image"});
+
+    });
+    console.log(name);
 })
 router.use(express.static(process.cwd()+'/public'));
 
